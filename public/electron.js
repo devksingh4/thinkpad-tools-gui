fdc038e8688a46ef567548e4788dd4db087c0f52
Dev Singh <dev@singhk.dev>
2020-05-06 17:06:12 -0500

    add ipc functionality
    Signed-off-by: Dev Singh <dev@singhk.dev>

--- public/electron.js
+++ public/electron.js
@@ -1,10 +1,11 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
+const {app, Notification, BrowserWindow, electron, dialog, ipcMain} = require('electron');
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
+
+const sudo = require('sudo-prompt');
+
+const { exec } = require('child_process');
 
 let mainWindow;
 
@@ -14,12 +15,94 @@ function createWindow() {
   mainWindow.on('closed', () => mainWindow = null);
 }
 
-app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
+app.on('browser-window-created', async () => {
+  if (process.platform !== "linux") {
+      const options = {
+          type: 'error',
+          buttons: ['OK'],
+          defaultId: 2,
+          title: 'Error: OS not supported',
+          message: 'OS not supported',
+         };
+      
+        await dialog.showMessageBox(null, options, console.log("notified user no thinkpad-tools in path"));
+        app.exit(0)
   }
+  if (!shell.which("thinkpad-tools")) {
+      const options = {
+          type: 'error',
+          buttons: ['OK'],
+          defaultId: 2,
+          title: 'Error: thinkpad-tools not found',
+          message: 'thinkpad-tools not found in PATH',
+          detail: 'Please ensure that thinkpad-tools is installed and that it is in your PATH',
+         };
+      
+        await dialog.showMessageBox(null, options, console.log("notified user no thinkpad-tools in path"));
+        app.exit(0)
+  }
+  
+})
+
+
+app.on('ready', createWindow);
+var options = {
+  name: 'ThinkpadToolsGUITrackpoint',
+};
+ipcMain.on('get-tp-trackpoint-status', (event, arg) => {
+  out = ""
+  sudo.exec('$(which thinkpad-tools) trackpoint status', {name: "Thinkpad Tools GUI"}, (error, stdout, stderr) => {
+      if (error) {
+          event.returnValue = {errormsg: error}
+          throw error
+      } else {
+          event.returnValue = {sensitivity: parseInt(stdout.match(/\d+/g)[0]), speed: parseInt(stdout.match(/\d+/g)[1])}
+      }
+  }
+);
+})
+
+ipcMain.on('set-tp-settings', (event, arg) => {
+  sudo.exec('$(which thinkpad-tools) trackpoint set-speed ' + arg.speed + ' && $(which thinkpad-tools) trackpoint set-sensitivity ' + arg.sensitivity , {name: "Thinkpad Tools GUI"}, (error, stdout, stderr) => {
+      if (error) {
+          event.returnValue = {errormsg: error}
+          throw error
+      } else {
+          event.returnValue = "done"
+      }
+  }
+);
+})
+
+ipcMain.on('get-list-bats', (event, arg) => {
+  exec('thinkpad-tools battery list', (err, stdout, stderr) => {
+      event.returnValue = stdout.replace(/\s/g, '')
+    });
+})
+
+ipcMain.on('get-battery-status', (event, arg) => {
+  exec('thinkpad-tools battery status', (err, stdout, stderr) => {
+      lines = stdout.match(/[^\r\n]+/g)
+      vals = []
+      for (i = 0; i < lines.length; i++) {
+          vals[i] = lines[i].split(':')[1].replace(/\s/g, '')
+      }
+      event.returnValue = vals
+  });
+})
+ipcMain.on('get-battery-tags', (event, arg) => {
+  exec('thinkpad-tools battery status', (err, stdout, stderr) => {
+      lines = stdout.match(/[^\r\n]+/g)
+      vals = []
+      for (i = 0; i < lines.length; i++) {
+          vals[i] = lines[i].split(':')[0]
+      }
+      event.returnValue = vals
+  });
+})
+app.on('window-all-closed', () => {
+    app.quit();
 });
 
 app.on('activate', () => {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
 import {
   HashRouter,
   Route
@@ -17,15 +15,10 @@ ReactDOM.render(
     <HashRouter>
       <div>
         <Route path="/" exact     component={ Home } />
-        <Route path="/trackpoint"     component={ Battery } />
+        <Route path="/trackpoint"     component={ Trackpoint } />
         <Route path="/battery"     component={ Battery } />
       </div>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
